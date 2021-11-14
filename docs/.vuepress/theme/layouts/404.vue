4ef503b6561f99d2e31d67d4f24ec9687bdac8ab
柳问星 <lwx_redstone@163.com>
2021-11-14 20:33:01 +0800

    自定义404页面加500ms延时

--- docs/.vuepress/theme/layouts/404.vue
+++ docs/.vuepress/theme/layouts/404.vue
@@ -1,10 +1,14 @@
 <template>
-    <Page404></Page404>
+    <Page404 v-show="show"></Page404>
 </template>
 
 <script setup lang="ts">
 import Page404 from '@vuepress/theme-default/lib/client/layouts/404.vue'
-
+import { ref } from 'vue';
+const show = ref(false)
+setTimeout(() => {
+    show.value = true
+}, 500)
 </script>
 
 <style lang="less">
